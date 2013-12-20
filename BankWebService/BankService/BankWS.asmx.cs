56ac82f0326ac39245eeb976d72ac65ea8c6a15a
roughee <roughee@outlook.com>
2013-12-20 14:30:59 +0100

    check for cpr and return error object
    if regex match fails it will return error object with error message

--- BankWebService/BankService/BankWS.asmx.cs
+++ BankWebService/BankService/BankWS.asmx.cs
@@ -7,6 +7,7 @@
 using System;
 using System.ServiceModel;
 using System.ServiceModel.Web;
+using System.Text.RegularExpressions;
 using System.Web.Script.Services;
 using System.Web.Services;
 
@@ -24,16 +25,29 @@ namespace BankService
         ParameterStyle = System.Web.Services.Protocols.SoapParameterStyle.Wrapped)]
         [ScriptMethod(UseHttpGet = true)]
         [OperationContract, WebInvoke(Method = "GET", UriTemplate = "LoanResponse?ssn={ssn}&creditScore={creditScore}&loanAmount={loanAmount}&loanDuration={loanDuration}")]
-        public InterestRate LoanResponse()
+        public Object LoanResponse()
         {
             var ssn = Convert.ToString(Context.Request["ssn"]);
             var creditScore = Convert.ToInt32(Context.Request["creditScore"]);
             var loanAmount = Convert.ToInt32(Context.Request["loanAmount"]);
             var loadDuration = Convert.ToInt32(Context.Request["loanDuration"]);
 
+            var ssnCprMatch = Regex.Match(ssn, @"^(0[1-9]|[12]\d|3[01])((0[1-9])|(1[0-2]))[0-9]{2}(\Q-\E)?[0-9]{4}$");
+
+            if (!ssnCprMatch.Success)
+            {
+                return new LoanValidationError { ErrorMessage = string.Format("Invalid ssn : {0}", ssn) };
+            }
+
+
             var loanCalculator = new LoanCalculator();
 
             return loanCalculator.CalculateLoan(ssn, creditScore, loanAmount, loadDuration);
         }
+    }
+
+    public class LoanValidationError
+    {
+        public String ErrorMessage { get; set; }
     }
 }
